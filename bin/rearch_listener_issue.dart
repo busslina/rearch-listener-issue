1d12664ce9822c769220e827f14ae292245e2d14
busslina <busslina@gmail.com>
2024-02-05 06:48:29 +0100

    initial commit

---
+++ bin/rearch_listener_issue.dart
@@ -0,0 +1,5 @@
+import 'package:rearch_listener_issue/rearch_listener_issue.dart' as rearch_listener_issue;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rearch_listener_issue.calculate()}!');
+}
---
+++ test/rearch_listener_issue_test.dart
@@ -0,0 +1,8 @@
+import 'package:rearch_listener_issue/rearch_listener_issue.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
